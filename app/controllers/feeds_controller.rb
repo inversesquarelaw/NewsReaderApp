bb3940b602af258055593fd78826ab793c724308
Tommy Duek <taduek@gmail.com>
2013-10-02 16:53:47 -0400

    Tidy up syntax in FeedsController#create.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -11,7 +11,7 @@ class FeedsController < ApplicationController
     feed = Feed.find_or_create_by_url(params[:feed][:url])
     if feed
       feed.reload
-      render :json => feed.to_json(include: {entries: {}})
+      render :json => feed.to_json(include: :entries)
     else
       render :json => { error: "invalid url" }, status: :unprocessable_entity
     end
