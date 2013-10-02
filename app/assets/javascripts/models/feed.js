feba1f6426a5185f8326d97e49f0000e39a01df1
Tommy Duek <taduek@gmail.com>
2013-10-02 17:42:20 -0400

    Cleanup, cleanup, everybody, everywhere.

--- app/assets/javascripts/models/feed.js
+++ app/assets/javascripts/models/feed.js
@@ -1,10 +1,8 @@
 NewReader.Models.Feed = Backbone.Model.extend({
   parse: function(response) {
-    if (response["entries"]) {
-      response["entries"] = new NewReader.Collections.Entries(response["entries"], {
-        url: "/feeds/" + response["id"] + "/entries"
-      })
-    }
+    response["entries"] = new NewReader.Collections.Entries(response["entries"], {
+      url: "/feeds/" + response["id"] + "/entries"
+    })
 
     return response;
   }
