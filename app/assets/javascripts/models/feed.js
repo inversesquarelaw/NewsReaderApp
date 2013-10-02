b3e181710a99195aa9ea9af841c2abd5441a5a0f
Tommy Duek <taduek@gmail.com>
2013-10-02 16:51:35 -0400

    Fix backbone Feed#parse and API FeedsController#index.

--- app/assets/javascripts/models/feed.js
+++ app/assets/javascripts/models/feed.js
@@ -1,12 +1,11 @@
 NewReader.Models.Feed = Backbone.Model.extend({
   parse: function(response) {
-    var that = this;
-    _.each(_.keys(response), function(key){
-      if (key !== "entries")
-        that.set(key, response[key]);
-    });
-    that.set("entries", new NewReader.Collections.Entries(response["entries"], {
-      url:"/feeds/" + response["id"] + "/entries"
-    }))
+    if (response["entries"]) {
+      response["entries"] = new NewReader.Collections.Entries(response["entries"], {
+        url: "/feeds/" + response["id"] + "/entries"
+      })
+    }
+
+    return response;
   }
 });
